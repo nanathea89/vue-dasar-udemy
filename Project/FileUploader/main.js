45f2b9e0c23ca143fe9d5b86f276b7b265469a8c
nanathea89 <m.nanamulyana@gmail.com>
2022-10-26 15:19:59 +0700

    js

--- Project/FileUploader/main.js
+++ Project/FileUploader/main.js
@@ -8,9 +8,9 @@ data(){
         selectedFile:'',
         selectedFileName:null,
         isUploadReadonly:false,
-        files:[],
+        // files:[],
         hoveredItem:null,
-        items:[
+        files:[
             {
                 id:1,
                 name:'Data Karyawan.xls',
@@ -33,22 +33,43 @@ methods:{
         this.progress=0;
         this.isUpload=true;
         this.uploadImg='loading.gif';
+        // const progressInterval = setInterval(()=>{
+        //     this.progress++;
+        //     if(this.progress==100){
+        //         clearInterval(progressInterval)
+        //         setTimeout(()=>{
+        //             this.files.push({
+        //                 id:this.files.length+1,
+        //                 name:this.selectedFileName
+        //             })
+        //             this.progress=0;
+        //             this.uploadImg=null;
+        //             this.isUpload=false;
+        //             this.selectedFile=null;
+        //             this.selectedFileName=null;
+        //             // this.progress=0
+        //         },1000);
+        //     }
+        // },50);
+
         const progressInterval = setInterval(()=>{
             this.progress++;
             if(this.progress==100){
-                clearInterval(progressInterval)
+
                 setTimeout(()=>{
-                    this.items.push({
-                        id:this.items.length+1,
+                    clearInterval(progressInterval)
+                    this.files.push({
+                        id:this.files.length+1,
                         name:this.selectedFileName
                     })
-                    // this.progress=0;
+                    this.progress=0;
                     this.uploadImg=null;
                     this.isUpload=false;
                     this.selectedFile=null;
                     this.selectedFileName=null;
-                    // this.progress=0
                 },1000);
+               
+
             }
         },50);
 
@@ -57,15 +78,23 @@ methods:{
         this.$refs.fileUpload.click();
     },
     changeUpload(){
-        this.files = this.$refs.fileUpload.files;
-        this.selectedFile = this.files[0];
-        this.selectedFileName = this.files[0].name;
+        this.file = this.$refs.fileUpload.files;
+        this.selectedFile = this.file[0];
+        this.selectedFileName = this.file[0].name;
     },
     cancelUpload(){
         this.selectedFile='';
         this.selectedFileName=null;
         this.isUpload=false;
-        this.files=[];
+        // this.files=[];
+    },
+    handleRemove(file){ 
+        if(confirm('are you sure?')){
+           const index = this.files.findIndex(file =>file.id == file.id)
+           this.files.splice(index,1);
+           console.log(index)
+           console.log(this.files)
+        }
     }
 }
 }).mount('#app');
